ebc7b454c81b97865ff6a1fe10f3763f05fded8e
-Altair- <115722320+Altairu@users.noreply.github.com>
2024-09-27 21:42:55 +0900

    Update AltairSerial.h

--- Altair_library_for_mbed/AltairSerial.h
+++ Altair_library_for_mbed/AltairSerial.h
@@ -18,7 +18,7 @@ public:
                 serial = new BufferedSerial(PA_9, PA_10, baudRate);
                 break;
             case USB_B:
-                serial = new BufferedSerial(PC_11, PC_10, baudRate);
+                serial = new BufferedSerial(PC_10, PC_11, baudRate);
                 break;
             case USB_MiniB:
                 serial = new BufferedSerial(USBTX, USBRX, baudRate);
