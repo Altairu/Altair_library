86c93bd82c76db6606a53beb95b49752369d98fc
-Altair- <115722320+Altairu@users.noreply.github.com>
2024-09-27 21:42:19 +0900

    Update AltairSerial.h

--- Altair_library_for_mbed/AltairSerial.h
+++ Altair_library_for_mbed/AltairSerial.h
@@ -2,6 +2,7 @@
 #define ALTAIR_SERIAL_H
 
 #include "mbed.h"
+#include <cstring>  // memcpyを使うため
 
 enum USBMode {
     USB_A,
@@ -25,24 +26,45 @@ public:
         }
     }
 
+    // Float型配列をヘッダーとともに送信
     void sendFloatArrayWithHeader(float* data, int length) {
         uint8_t header = 0xA5;
-        serial->write(&header, 1);
-        serial->write(data, length * sizeof(float));
+        serial->write(&header, 1);  // ヘッダー送信
+        for (int i = 0; i < length; i++) {
+            sendFloat(data[i]);  // 各floatを送信
+        }
     }
 
+    // ヘッダー付きのFloat型配列を受信
     void receiveFloatArrayWithHeader(float* buffer, int& length) {
         uint8_t header;
         do {
-            serial->read(&header, 1);
+            serial->read(&header, 1);  // ヘッダー待ち
         } while (header != 0xA5);
 
-        serial->read(buffer, length * sizeof(float));
+        for (int i = 0; i < length; i++) {
+            buffer[i] = receiveFloat();  // 各floatを受信
+        }
     }
 
 private:
     BufferedSerial* serial;
+
+    // Floatをバイト列に変換して送信
+    void sendFloat(float value) {
+        uint8_t floatBytes[sizeof(float)];
+        memcpy(floatBytes, &value, sizeof(float));
+        serial->write(floatBytes, sizeof(float));
+    }
+
+    // バイト列をFloatに変換して受信
+    float receiveFloat() {
+        uint8_t floatBytes[sizeof(float)];
+        serial->read(floatBytes, sizeof(float));
+        float value;
+        memcpy(&value, floatBytes, sizeof(float));
+        return value;
+    }
 };
 
 #endif
-
