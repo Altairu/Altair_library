05939cb997b68251e5d0651009a301dfff34aa0e
Altairu <Altairu@github.comu>
2024-11-08 14:37:49 +0900

    new

--- Altair_library_for_CubeIDE/kinematics.h
+++ Altair_library_for_CubeIDE/kinematics.h
@@ -1,59 +1,24 @@
-#ifndef KINEMATICS_H_
-#define KINEMATICS_H_
+// kinematics.h
 
-#include "motor_driver.h"
-#include "encoder.h"
-#include "pid.h"
-#include "math.h"
+#ifndef KINEMATICS_H
+#define KINEMATICS_H
 
-// 制御モードの定義
-typedef enum {
-    PWM_MODE,
-    PID_MODE
-} KinematicsMode;
-
-// ホイールの動作モードを定義
 typedef enum {
     OMNI_3,
     OMNI_4,
     MEKANUM
 } WheelMode;
 
-// ホイールの配置場所を定義
-typedef enum {
-    FRONT_RIGHT,
-    FRONT_LEFT,
-    BACK_RIGHT,
-    BACK_LEFT
-} WheelPosition;
-
-// デバッグデータ用の構造体
 typedef struct {
-    double enc[4];
-    double out[4];
-    double target[4];
-} DebugData;
-
-// 足回りの運動学を管理する構造体
-typedef struct {
-    MotorDriver motors[4];
-    Encoder encoders[4];
-    Pid pid_controllers[4];
-    DebugData debug_data;
-    double lx, ly, rx;
-    double robot_diameter;
-    double wheel_radius;
-    KinematicsMode mode;  // 追加: モード設定
+    float robot_diameter;   // ロボットの直径
+    float wheel_radius;     // ホイールの半径
+    WheelMode mode;         // 動作モード
 } Kinematics;
 
-// 関数プロトタイプ
-void Kinematics_InitMotor(Kinematics* kin, WheelPosition pos, MotorDriver* motor,
-                          TIM_HandleTypeDef* htimA, uint32_t channelA,
-                          TIM_HandleTypeDef* htimB, uint32_t channelB);
-void Kinematics_InitEncoder(Kinematics* kin, WheelPosition pos, Encoder* encoder);
-void Kinematics_SetPIDGains(Kinematics* kin, WheelPosition pos, double kp, double ki, double kd);
-void Kinematics_SetMode(Kinematics* kin, KinematicsMode mode);
-void Kinematics_Control(Kinematics* kin, WheelMode mode, double lx, double ly, double rx);
-DebugData Kinematics_GetDebugData(Kinematics* kin);
+// 初期化関数
+void Kinematics_Init(Kinematics *kinematics, float robot_diameter, float wheel_radius, WheelMode mode);
 
-#endif /* KINEMATICS_H_ */
+// モーターの目標速度を計算する関数
+void Kinematics_GetTargetSpeeds(Kinematics *kinematics, float lx, float ly, float rx, float *speedFR, float *speedFL, float *speedBR, float *speedBL);
+
+#endif // KINEMATICS_H
